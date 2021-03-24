4dcb0e978bc30c0587ad82c37035239997b63d31
komca12 <57370991+komca12@users.noreply.github.com>
2021-03-24 23:12:18 +0100

    kek

--- interperet.py
+++ interperet.py
@@ -9,13 +9,16 @@ import xml.dom.minidom as minidom
 import xml.etree.ElementTree as ET
 
 def main():
-    INPUT,SOURCE=parametersParse(sys.argv)
-    tree=xmlTreeParsing(SOURCE)
-    root=tree.getroot()
-    MyProgram=programParsing(root)
+    INPUT,SOURCE=parametersParse(sys.argv)      #zpracování parametrů
+    tree=xmlTreeParsing(SOURCE)                 #získání ze xml stromovou strukturu
+    root=tree.getroot()                         
+    MyProgram=programParsing(root)              #zpracování xml stromu 
 
     print(MyProgram)
-    MyProgram.firstrun()
+    MyProgram.firstrun()                        #první průchod programem-kontrola operátárů a jejich parametů(hlavně syntaktické kontroly), zaznammání pozic skoků
+    #MyProgram.secondrun()                      #druhý průchod programem-samotné provedení instrukcí
+
+    error(0,"program run succesfuly")
     exit(0)
 class Instrucrion(object):
     Type=""
@@ -25,34 +28,55 @@ class Instrucrion(object):
         self.args=args
     def __repr__(self):
         return "<Instruction - type: %s,\t args:%s >\n" % (self.Type, self.args)
-    def selfcheck(self):
+    def selfCheck(self):    #syntaktická kontrola instrukcí
         op=self.Type
         if(op=="RETURN"or op=="CREATEFRAME"or op=="PUSHFRAME"or op=="POPFRAME"or op=="BREAK"):
-            pass
+            self.paramCheck()
         elif(op=="ADD"or op=="SUB"or op=="MUL"or op=="IDIV"or op=="LT"or op=="GT"or op=="EQ"or op=="AND"or op=="OR"or op=="NOT"or op=="STRI2INT"or op=="CONCAT"or op=="GETCHAR"or op=="SETCHAR"):
-			pass
+            self.paramCheck("VAR","SYM","SYM")
         elif(op=="MOVE"):
-			pass
+            self.paramCheck("VAR","SYM")
         elif(op=="INT2CHAR"or op=="STRLEN"or op=="TYPE"):
-			pass
+            self.paramCheck("SYM","SYM")
         elif(op=="DEFVAR"or op=="POPS"):
-			pass
+            self.paramCheck("VAR")
         elif(op=="JUMPIFEQ"or op=="JUMPIFNEQ"):
-            pass
+            self.paramCheck("LABEL","SYM","SYM")
         elif(op=="LABEL"or op=="CALL"or op=="JUMP"):
-            pass
+            self.paramCheck("LABEL")
         elif(op=="PUSHS"or op=="WRITE"or op=="EXIT"or op=="DPRINT"):
-			pass
+            self.paramCheck("SYM")
         elif(op=="READ"):
-			pass
+            self.paramCheck("VAR","TYPE")
         else:
-			error(21,"Unknown op Code")
-
+            error(21,"Unknown op Code")
+    def paramCheck(self,*args):     #porovnání typů parametrů instrukcí s očekávanými typy
+        if(len(args)!=len(self.args)):
+            error(-1,"to few or to much paremeters on instruction")
+        paramCounter=0
+        for arg in args:
+            paramCounter+=1
+            if(not(str(paramCounter) in self.args)):
+                error(32,"parameter not found")
+            Type=self.args[str(paramCounter)].Type
+            if(arg=="SYM"):
+                if(not(Type=="BOOL" or Type=="VAR" or Type=="STRING" or Type=="INT" or Type=="NIL")):
+                    error(-1,"arg check error")
+            elif(Type!=arg):
+                error(-1,"arg check error")
 class operant(object):
     Type=""
     value=""
+    placement=None
     def __init__(self,Type,value):
         self.Type=Type.upper()
+        if(self.Type=="VAR"):
+            separated=value.split('@',1)
+            if((separated[0]=="GF" or separated[0]=="LF" or separated[0]=="TF")and (separated[1]!="")):
+                value=separated[1]
+                self.placement=separated[0]
+            else:    
+                error(32,"Varieble format not valid")     
         self.value=value
     def __repr__(self):
         return "<Operant - type: %s, value: %s>" % (self.Type, self.value)
@@ -64,10 +88,9 @@ class program(object):
         return "<Program: - instructions:\n %s" % (self.instructructions)    
     def firstrun(self):
         for i in range(1, len(self.instructructions)+1):
-            self.instructructions[str(i)]
+            self.instructructions[str(i)].selfCheck()
         return
-
-def parametersParse(argv): 
+def parametersParse(argv):  #funkce pro zpracování parametrů
     INPUT=None
     SOURCE=None
     if(len(argv)==2 or len(argv)==3):
