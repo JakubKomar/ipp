07ca73ac4a634da608f164d50770e94af32f0809
komca12 <57370991+komca12@users.noreply.github.com>
2021-03-24 17:43:03 +0100

    Update interperet.py

--- interperet.py
+++ interperet.py
@@ -11,7 +11,6 @@ import xml.etree.ElementTree as ET
 class Instrucrion(object):
     Type=""
     args={}
-
     def __init__(self,Type,args):
         self.Type=Type
         self.args=args
@@ -25,11 +24,12 @@ class operant(object):
         self.value=value
     def __repr__(self):
         return "<Operant - type: %s, value: %s>" % (self.Type, self.value)
-
-        
-
-
-
+class program(object):   
+    instructructions={}
+    def __init__(self,instructructions):
+        self.instructructions=instructructions
+    def __repr__(self):
+        return "<Program: - instructions:\n %s" % (self.instructructions)    
 def parametersParse(argv): 
     INPUT=None
     SOURCE=None
@@ -64,41 +64,45 @@ def xmlTreeParsing(SOURCE):
         print("bad xml structure")
         exit(31)
     return tree
-
-
+def programParsing(root):
+    if(root.tag!="program"):
+        print("root tag err")
+        exit(32)
+    childs=list(root)
+    childsLen=len(childs)
+    instructions={}
+    for child in childs:
+        if(child.tag!="instruction"):
+            print("unknown child in xml")
+            exit(32)
+        if(not("order" in child.attrib)or not("opcode" in child.attrib)):
+            print("missing instruction atribut")
+            exit(32)
+        if(int(child.attrib["order"])<1 or int(child.attrib["order"])>childsLen or child.attrib["order"] in instructions):
+            print("instruction order not valid")
+            exit(32)
+        args={}
+        for arg in child:
+            if(arg.tag[0:3]!="arg"):
+                print("unknown format of parameter")
+                exit(32)
+            if(not("type" in arg.attrib)):
+                print("missing parameter atribut")
+                exit(32)
+            if(int(arg.tag[3:])<0 or int(arg.tag[3:])>3 or arg.tag[3:] in arg):
+                print("order of operand is not valid")
+                exit(32)
+            args[arg.tag[3:]]=operant(arg.attrib["type"],arg.text)
+        instructions[child.attrib["order"]]=Instrucrion(child.attrib["opcode"],args)
+    return program(instructions)
 
 INPUT,SOURCE=parametersParse(sys.argv)
 tree=xmlTreeParsing(SOURCE)
-
-##########main part###########
 root=tree.getroot()
-if(root.tag!="program"):
-    print("root tag err")
-    exit(32)
-childs=list(root)
-childsLen=len(childs)
-program={}
-for child in childs:
-    if(child.tag!="instruction"):
-        print("unknown child in xml")
-        exit(32)
-    if(int(child.attrib["order"])<1 or int(child.attrib["order"])>childsLen):
-        print("order not valid")
-        exit(32)
-    if(child.attrib["order"] in program):
-        print("duplicid istruction")
-        exit(32)
-    args={}
-    for arg in child:
-        if(arg.tag[0:3]!="arg"):
-            print("unknown format of parameter")
-            exit(32)
-        if(int(arg.tag[3:])<0 or int(arg.tag[3:])>3):
-            print("order of operand is not valid")
-            exit(32)
-        args[arg.tag[3:]]=operant(arg.attrib["type"],arg.text)
-    program[child.attrib["order"]]=Instrucrion(child.attrib["opcode"],args)
-print(program)
+MyProgram=programParsing(root)
+
+print(MyProgram)
+
 
 
 
