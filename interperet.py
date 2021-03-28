9406f18f88be0f91b4761c94242362f398b2d141
komca12 <57370991+komca12@users.noreply.github.com>
2021-03-28 16:10:22 +0200

    +-

--- interperet.py
+++ interperet.py
@@ -16,9 +16,7 @@ def main():
     MyProgram=programParsing(root)              #zpracování xml stromu 
 
     MyProgram.firstRun()                        #první průchod programem-kontrola operátárů a jejich parametů(hlavně syntaktické kontroly), zaznammání pozic skoků
-    print(MyProgram)
     MyProgram.secondRun()                      #druhý průchod programem-samotné provedení instrukcí
-    print(MyProgram.mem)
     error(0,"program run succesfuly")
     exit(0)
 class Instrucrion(object):
@@ -29,9 +27,9 @@ class Instrucrion(object):
         self.args=args
         if(op=="RETURN"or op=="CREATEFRAME"or op=="PUSHFRAME"or op=="POPFRAME"or op=="BREAK"):
             self.paramCheck()
-        elif(op=="ADD"or op=="SUB"or op=="MUL"or op=="IDIV"or op=="LT"or op=="GT"or op=="EQ"or op=="AND"or op=="OR"or op=="NOT"or op=="STRI2INT"or op=="CONCAT"or op=="GETCHAR"or op=="SETCHAR"):
+        elif(op=="ADD"or op=="SUB"or op=="MUL"or op=="IDIV"or op=="LT"or op=="GT"or op=="EQ"or op=="AND"or op=="OR"or op=="STRI2INT"or op=="CONCAT"or op=="GETCHAR"or op=="SETCHAR"):
             self.paramCheck("var","sym","sym")
-        elif(op=="MOVE"):
+        elif(op=="MOVE" or op=="NOT"):
             self.paramCheck("var","sym")
         elif(op=="INT2CHAR"or op=="STRLEN"or op=="type"):
             self.paramCheck("sym","sym")
@@ -64,7 +62,6 @@ class Instrucrion(object):
                     error(-1,"arg check error")
             elif(Type!=arg):
                 error(-1,"arg check error")
-
 class operant(object):
     Type=""
     value=""
@@ -74,7 +71,7 @@ class operant(object):
         if(self.Type=="var"):
             separated=value.split('@',1)
             if((separated[0]=="GF" or separated[0]=="LF" or separated[0]=="TF")and (separated[1]!="")):
-                if not re.match(r"^[A-Za-z_\-$&%*!?][A-Za-z0-9_\-$&%*!?]*$", separated[1]):
+                if not re.match(r"^[A-Za-z_\-$&%;*!?][A-Za-z0-9_\-$&%;*!?]*$", separated[1]):
                     error(32,"variable name invalid")
                 value=separated[1]
                 self.placement=separated[0]
@@ -89,7 +86,10 @@ class operant(object):
                 error(32,"Bool value not valid")
         elif(self.Type=="int"):
             if(re.match(r"^((\+|\-)?(0|[1-9][0-9]*))$", value)):
-                value=int(value)
+                try:
+                    value=int(value)
+                except ValueError:
+                    error(32,"Int coverzion error after regex checks")
             else:
                 error(32,"INT value not valid")
         elif(self.Type=="string"):
@@ -100,7 +100,7 @@ class operant(object):
             if(not re.match(r"^(nil|bool|int|string)$", value)):
                 error(32,"Type invalid")
         elif(self.Type=="label"):
-            if(not re.match(r"^([A-Za-z_\-$&%*!?][A-Za-z0-9_\-$&%*!?]*)$", value)):
+            if(not re.match(r"^([A-Za-z_\-$&;%*!?][A-Za-z0-9_\-$&%;*!?]*)$", value)):
                 error(32,"Label value not valid")
         else:   
             error(32,"unknown type")
@@ -127,7 +127,10 @@ class memory(object):
     TF=None
     LF=None
     stack=[]
-    def writeToVAR(self,name,frameType,Value):
+    varStack=[]
+    def writeToVAR(self,arg,Value):
+        frameType=arg.placement
+        name=arg.value
         if frameType=="GF":
             if not name in self.GF.varS:
                 error(52,"Moving destination doesnt exist in GF")
@@ -146,8 +149,10 @@ class memory(object):
             self.TF.varS[name]=Value
         else:
             error(99,"this frame dont exists")
-    def readFromVar(self,name,frameType):
+    def readFromVar(self,arg):
         Value=None
+        name=arg.value
+        frameType=arg.placement
         if frameType=="GF":
             if not name in self.GF.varS:
                 error(52,"Var  doesnt exist in GF")
@@ -204,7 +209,7 @@ class memory(object):
         else:
             error(99,"defvar err")
     def MOVE(self,args):
-        self.writeToVAR(args["1"].value,args["1"].placement,args["2"].value,)
+        self.writeToVAR(args["1"],args["2"].value)
     def __repr__(self):
         GF=frame()
         TF=None
@@ -219,8 +224,13 @@ class program(object):
     jTable=jumpTable()
     mem=memory()
     counter=1
+    procesedInstruct=0
     def __init__(self,instructructions):
         self.instructructions=instructructions
+        self.jTable=jumpTable()
+        self.mem=memory()
+        self.counter=1
+        self.procesedInstruct=0
     def __repr__(self):
         return "<Program: - instructions:\n %s \nJumptable:\n %s" % (self.instructructions,self.jTable)    
     def firstRun(self):
@@ -233,6 +243,7 @@ class program(object):
                     self.jTable.table[name]=i
     def secondRun(self):
         while self.counter<=len(self.instructructions):
+            self.procesedInstruct+=1
             self.execute(self.instructructions[str(self.counter)])
             self.counter+=1
     def execute(self,instruction):
@@ -252,31 +263,31 @@ class program(object):
         elif instruction.Type=="RETURN":
             self.RETURN()
         elif instruction.Type=="PUSHS":
-            pass
+            self.PUSHS(instruction.args)
         elif instruction.Type=="POPS":
-            pass
+            self.POPS(instruction.args)
         elif instruction.Type=="ADD":
-            pass
+            self.ARITMETIC(instruction.args,"ADD")
         elif instruction.Type=="SUB":
-            pass
+            self.ARITMETIC(instruction.args,"SUB")
         elif instruction.Type=="MUL":
-            pass
+            self.ARITMETIC(instruction.args,"MUL")
         elif instruction.Type=="IDIV":
-            pass
+            self.ARITMETIC(instruction.args,"IDIV")
         elif instruction.Type=="LT":
-            pass
-        elif instruction.Type=="QT":
-            pass
+            self.COMPARISON(instruction.args,"LT")
+        elif instruction.Type=="GT":
+            self.COMPARISON(instruction.args,"GT")
         elif instruction.Type=="EQ":
-            pass
+            self.COMPARISON(instruction.args,"EQ")
         elif instruction.Type=="AND":
-            pass
+            self.BINARY(instruction.args,"AND")
         elif instruction.Type=="OR":
-            pass
+            self.BINARY(instruction.args,"OR")
         elif instruction.Type=="NOT":
-            pass
+            self.BINARY(instruction.args,"NOT")
         elif instruction.Type=="INT2CHAR":
-            pass
+            self.INT2CHAR(instruction.args)
         elif instruction.Type=="STRI2INT":
             pass
         elif instruction.Type=="READ":
@@ -304,14 +315,14 @@ class program(object):
         elif instruction.Type=="EXIT":
             self.EXIT(instruction.args)
         elif instruction.Type=="DPRINT":
-            self.DPRINT()
+            self.DPRINT(instruction.args)
         elif instruction.Type=="BREAK":
             self.BREAK()
         else:
             error(99,"unknow procedure for instruction execution")
     def WRITE(self,args):
         if(args["1"].Type=="var"):
-            print(self.mem.readFromVar(args["1"].value,args["1"].placement))
+            print(self.mem.readFromVar(args["1"]))
         else:
             print(args["1"].value) 
     def CALL(self,args):
@@ -330,16 +341,94 @@ class program(object):
             error(-1,"Jump label isnt in code")
         pass
     def DPRINT(self,args):
-        error(0,str(args["1"].value))
+        error(0,str(self.loadValue(args["1"])))
     def BREAK(self):
-        error(0,"Pozition: %d \n%s"% (self.counter,self))
+        error(0,"================================================\nDebug write\nPozition in code: %d\nCount of executed instructions: %d\nVAR stack:%s\nMemory:\n%s================================================"% (self.counter,self.procesedInstruct,self.mem.varStack,self.mem))
     def EXIT(self,args):
         retCode=0
         try:
-            retCode=int(args["1"].value)
+            retCode=int(self.loadValue(args["1"]))
         except:
             retCode=57
         exit(retCode)
+    def loadValue(self,arg):
+        if arg.Type=="var":
+            value=self.mem.readFromVar(arg)
+        else:
+            value=arg.value
+        return value
+    def PUSHS(self,args):
+        self.mem.varStack.append(self.loadValue(args["1"]))
+    def POPS(self,args):
+        if(not self.mem.varStack):
+            error(56,"Var stack is empty-cant pops")
+        self.mem.writeToVAR(args["1"],self.mem.varStack.pop(-1))
+    def ARITMETIC(self,args,operation):
+        a=self.loadValue(args["2"])
+        b=self.loadValue(args["3"])
+        c=None
+        if(type(a)!=type(b) or type(a)!=int):
+            error(-1,"aritmetic operants are not int")
+        if(operation=="ADD"):
+            c=a+b
+        elif(operation=="SUB"):
+            c=a-b
+        elif(operation=="MUL"):
+            c=a*b
+        elif(operation=="IDIV"):
+            if(b==0):
+                error(57,"DIViding by zero is permited")
+            c=a//b
+        else:
+            error(99,"aritmatic selector failed")
+        self.mem.writeToVAR(args["1"],c)
+    def COMPARISON(self,args,operation):
+        a=self.loadValue(args["2"])
+        b=self.loadValue(args["3"])
+        c=None
+        nilComp=(a==None or b==None)
+        if(type(a)!=type(b) and not nilComp):
+            error(53,"COMPARISON operants are not same")
+        if(operation=="EQ"):
+            c=a==b
+        elif not nilComp:
+            if(operation=="LT"):
+                c=a<b
+            elif(operation=="GT"):
+                c=a>b
+            else:
+                error(99,"aritmatic COMPARISON selector failed")
+        else:
+            error(53,"nil value can be compered only by equalition")
+        self.mem.writeToVAR(args["1"],c)
+    def BINARY(self,args,operation):
+        a=self.loadValue(args["2"])
+        c=None
+        if(operation!="NOT"):
+            b=self.loadValue(args["3"])
+            if(type(a)!=type(b) or type(a)!=bool):
+                error(-1,"BINARY operants are not bool")
+            if(operation=="AND"):
+                c=a and b
+            elif(operation=="OR"):
+                c=a or b
+        elif(operation=="NOT"):
+            if type(a)!=bool:
+                error(-1,"BINARY operant is not bool")
+            c=not a
+        else:
+            error(99,"Binary selector failed")
+        self.mem.writeToVAR(args["1"],c)
+    def INT2CHAR(self,args):
+        a=self.loadValue(args["2"])
+        c=None
+        if type(a)!=int:
+            error(58,"INT2CHAR err-operant is not int")
+        try:
+            c=chr(a)
+        except :
+            error(58,"INT2CHAR converzion error")
+        self.mem.writeToVAR(args["1"],c)
 
 
 def parametersParse(argv):  #funkce pro zpracování parametrů
@@ -352,9 +441,9 @@ def parametersParse(argv):  #funkce pro zpracování parametrů
                 print("Options:\n--source=\"file\"       =>  select source file \n--input=\"file\"       =>  select input file\n\nif one option missing the data are reading from STDIN")
                 exit(0)
             arg=arg.split('=',1)
-            if(arg[0]=="--source" and arg[1]!=None):
+            if(arg[0]=="--source" and len(arg)==2):
                 SOURCE=arg[1]
-            elif(arg[0]=="--input" and arg[1]!=None):
+            elif(arg[0]=="--input" and len(arg)==2):
                 INPUT=arg[1]
             else:
                 error(10,"wrong parameters, try --help")
@@ -383,15 +472,23 @@ def programParsing(root):
             error(32,"unknown child in xml")
         if(not("order" in child.attrib)or not("opcode" in child.attrib)):
             error(32,"missing instruction atribut")
-        if(int(child.attrib["order"])<1 or int(child.attrib["order"])>childsLen or child.attrib["order"] in instructions):
-            error(32,"instruction order not valid")
+        try:
+            order=int(child.attrib["order"])
+        except ValueError:
+            error(32,"atribut order converzion error")
+        if(order<1 or order>childsLen or child.attrib["order"] in instructions):
+            error(32,"instruction order in instruction not valid")
         args={}
         for arg in child:
             if(arg.tag[0:3]!="arg"):
                 error(32,"unknown format of parameter")
             if(not("type" in arg.attrib)):
                 error(32,"missing parameter atribut")
-            if(int(arg.tag[3:])<0 or int(arg.tag[3:])>3 or arg.tag[3:] in arg):
+            try:
+                ParamOrder=int(arg.tag[3:])
+            except ValueError:
+                error(32,"atribut order in arg converzion error")
+            if(ParamOrder<0 or ParamOrder>3 or arg.tag[3:] in arg):
                 error(32,"order of operand is not valid")
             args[arg.tag[3:]]=operant(arg.attrib["type"],arg.text)
         instructions[child.attrib["order"]]=Instrucrion(child.attrib["opcode"],args)
